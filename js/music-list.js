58637adf3f549ae662689963904be4ddbce7b9c2
Suraj <surajshukla15072000@gmail.com>
2022-09-21 16:03:06 +0530

    music-player

---
+++ js/music-list.js
@@ -0,0 +1,73 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Rimjhim Gire Sawan",
+    artist: "Kishore Kumar",
+    img: "1",
+    src: "1"
+  },
+  {
+    name: "Roop Tera Mastana",
+    artist: "Kishore Kumar",
+    img: "2",
+    src: "2"
+  },
+  {
+    name: "Pal Pal Dil Ke Paas",
+    artist: "Kishore Kumar",
+    img: "3",
+    src: "3"
+  },
+  {
+    name: "Salam-E-Ishq Meri Jaan",
+    artist: "Lata Mangeshkar",
+    img: "4",
+    src: "4"
+  },
+  {
+    name: "Dekha Ek Khwaab",
+    artist: "Lata Mangeshkar",
+    img: "5",
+    src: "5"
+  },
+  {
+    name: "Teri Ummed Tera Intezaar",
+    artist: "Kumar Sanu",
+    img: "6",
+    src: "6"
+  },
+  {
+    name: "Ye Sham Mastani",
+    artist: "Kishore Kumar",
+    img: "7",
+    src: "7"
+  },
+  {
+    name: "Rat Kali Ek Khwaab",
+    artist: "Kishore Kumar",
+    img: "8",
+    src: "8"
+  },
+  {
+    name: "Mera Dil Bhi Kitna",
+    artist: "Kumar Sanu, Alka Yagnik",
+    img: "9",
+    src: "9"
+  },
+  {
+    name: "Mausam Ki Tarah Tum Bhi",
+    artist: "Alka Yagnik",
+    img: "10",
+    src: "10"
+  }
+]
